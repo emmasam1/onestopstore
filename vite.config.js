7241001442876b82276764750f1c49145c0350b5
findm <fndm4@gmail.com>
2023-05-22 18:27:55 +0100

    fixing proxy issue

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//         output:{
+//             manualChunks(id) {
+//                 if (id.includes('node_modules')) {
+//                     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+//                 }
+//             }
+//         }
+//     }
+// }
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/": "http://localhost:5000",
+    },
+  },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-}
-})
+});
